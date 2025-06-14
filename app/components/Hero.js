1204b9775bc5bfa15d7a737ee65f98467aad0e83
denoire <thomaszac19@gmail.com>
2025-06-13 20:28:52 -0700

    Add website title and subtitle to hero

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,12 +1,20 @@
+import Image from 'next/image';
+import React from "react";
+
 export default function Hero() {
     return (
-        <section className="
+        <section
+            id="hero"
+            className="
             border border-red-500
             min-h-screen
             flex flex-col justify-center items-center text-center
             "
         >
-            hero
+            <h1 className="text-5xl mb-3">Thomas&apos; Personal Pages</h1>
+            <p className="italic">
+                A small archive of things that matter
+            </p>
         </section>
-    );
+);
 }
