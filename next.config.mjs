43969b9b7fe1fbc1c12e6b291a8f0a7acc4a730d
denoire <thomaszac19@gmail.com>
2025-06-16 16:15:28 -0700

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    trailingSlash: true,
-    images: {
-        unoptimized: true,
-    },
-    basePath: '',
-    assetPrefix: '',
 };
 
 export default nextConfig;
