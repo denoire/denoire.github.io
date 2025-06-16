e348d8500a2429088e9f72cba41fbe5fdc347eae
denoire <thomaszac19@gmail.com>
2025-06-15 23:26:42 -0700

    Add reusable Chapter component with title and text props

---
+++ app/components/Chapter.js
@@ -0,0 +1,18 @@
+export default function Chapter({title, text}) {
+    return (
+        <section
+            id={title}
+            className="
+            border border-red-500
+            min-h-screen
+            relative bg-[#fdf6e3]
+            flex flex-col justify-center items-center text-center
+            "
+        >
+            <div className="text-center">
+                <h2 className="text-4xl font-bold mb-3">{title}</h2>
+                <p className="text-lg italic text-gray-500">{text}</p>
+            </div>
+        </section>
+    );
+}
