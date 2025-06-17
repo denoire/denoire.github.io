a83bbecddfc107b82888df56fe3e74ce79b51b9c
denoire <thomaszac19@gmail.com>
2025-06-16 19:31:37 -0700

    Modify Chapter component

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Section from "./components/Section.js";
 import Closing from "./components/Closing.js";
 import Divider from "./components/Divider.js";
+import Chapter from "./components/Chapter";
 
 export default function Home() {
   return (
@@ -13,6 +14,16 @@ export default function Home() {
               <Hero />
               <Divider />
 
+              <Chapter
+                  title="Literature"
+                  text="I’ve always paid attention to the architecture of thought. Writing is an abstraction of the
+                    mind—a deliberate act of selection. And like any structure built under tension, sentences are fragile
+                    by design; a single imbalances echoes through the rest. Even now, I read like a writer and write like
+                    a reader, because I admire literature that reads like thought and moves with a certain intimacy. I’m
+                    drawn to pieces that slow me down and leaves space for silence, for doubt. I don’t mind when a text
+                    asks more questions than it answers, or if its beauty lies in the stillness of reflection. I find that
+                    quite elegant. Here are some such pieces that I find myself returning to."
+              />
               <About />
               <Divider />
 
