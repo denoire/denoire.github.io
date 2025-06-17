336f48ad5a72695b5bb00a3d8227b1fbf6deef12
denoire <thomaszac19@gmail.com>
2025-06-16 20:24:46 -0700

    Add page-like background and title

--- app/components/About.js
+++ app/components/About.js
@@ -5,10 +5,13 @@ export default function About() {
             className="
             border border-red-500
             min-h-screen
+            relative bg-[#fdf6e3]
             flex flex-col justify-center items-center text-center
             "
         >
-            about
+            <div className="text-center">
+                <h2 className="text-4xl font-bold mb-5">About</h2>
+            </div>
         </section>
     );
 }
