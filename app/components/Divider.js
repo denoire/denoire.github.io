e46302a622ebbab0a7b7b9e130a53115e2bcb01c
denoire <thomaszac19@gmail.com>
2025-06-18 13:54:28 -0700

    Implement divider.svg

--- app/components/Divider.js
+++ app/components/Divider.js
@@ -1,9 +1,13 @@
-import Image from 'next/image';
-
 export default function Divider() {
     return (
-        <div className="border border-red-500 w-full flex justify-center">
-            divider
+        <div className="w-full flex justify-center">
+            <img
+                src="/svgs/divider.svg"
+                alt="divider"
+                width={400}
+                height={60}
+                className="w-2/3 border border-red-500"
+            />
         </div>
     );
 }
