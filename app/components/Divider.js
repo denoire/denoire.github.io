e8143ac694687b962e3d3b7b33024514006a8c44
denoire <thomaszac19@gmail.com>
2025-06-18 14:07:26 -0700

    Remove guide borders

--- app/components/Divider.js
+++ app/components/Divider.js
@@ -1,12 +1,12 @@
 export default function Divider() {
     return (
-        <div className="p-15 border border-red-500 flex justify-center">
+        <div className="p-15 flex justify-center">
             <img
                 src="/svgs/divider.svg"
                 alt="divider"
                 width={400}
                 height={60}
-                className="p-5 w-full border border-red-500"
+                className="p-5 w-full"
             />
         </div>
     );
