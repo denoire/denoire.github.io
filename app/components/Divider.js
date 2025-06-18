2a7fea11fcf7ddd868878f8e78eab8b4980a82a8
denoire <thomaszac19@gmail.com>
2025-06-18 14:04:04 -0700

    Update divider spacing

--- app/components/Divider.js
+++ app/components/Divider.js
@@ -1,12 +1,12 @@
 export default function Divider() {
     return (
-        <div className="w-full flex justify-center">
+        <div className="p-15 border border-red-500 flex justify-center">
             <img
                 src="/svgs/divider.svg"
                 alt="divider"
                 width={400}
                 height={60}
-                className="w-2/3 border border-red-500"
+                className="p-5 w-full border border-red-500"
             />
         </div>
     );
