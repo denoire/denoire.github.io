9ef173526de35dbdb0694735d40d59caeb1f2d2a
denoire <thomaszac19@gmail.com>
2025-06-18 21:26:51 -0700

    Add reuseable custom hyperlink component

---
+++ app/components/CustomLink.js
@@ -0,0 +1,38 @@
+"use client"
+import { useState } from 'react';
+
+function CustomLink({ text, href, bubbleText }) {
+    const [hovering, setHovering] = useState(false);
+
+    return (
+        <span
+            onMouseEnter={() => setHovering(true)}
+            onMouseLeave={() => setHovering(false)}
+            className="
+                relative
+                cursor-pointer
+                underline
+                decoration-double
+                decoration-yellow-400
+                underline-offset-2
+                bg-yellow-200
+                transition
+                duration-900"
+        >
+            {text}
+            {hovering && bubbleText && (
+                <span className="
+                    absolute
+                    bottom-full left-1/2 -translate-x-1/2
+                    border border-gray-300
+                    rounded-lg bg-white shadow-md
+                    px-3 py-2 mb-2
+                    text-sm text-gray-600">
+                    {bubbleText}
+                </span>
+            )}
+        </span>
+    );
+}
+
+export default CustomLink;
