d58c9b9018f22e858f93bf86545a5f78ed63fc3d
denoire <thomaszac19@gmail.com>
2025-06-18 23:36:13 -0700

    Implement working internal section links in Sidebar

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,14 +1,26 @@
 export default function Sidebar() {
-    const sections = ["about", "chapter1", "chapter2", "chapter3", "closing"];
+    const sections = ["about", "literature", "activities", "inspirations", "closing"];
 
     return (
         <nav className="
+            z-1
             border border-red-500
             fixed flex items-center
             left-6 top-1/2 -translate-y-1/2 h-2/3
             "
         >
-            sidebar
+            <ul className="border border-red-500 flex flex-col text-center space-y-15">
+                {sections.map((section) => (
+                    <li key={section}>
+                        <a
+                            href={`#${section}`}
+                            className="border border-red-500 text-sm hover:underline"
+                        >
+                            {section}
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </nav>
     );
 }
