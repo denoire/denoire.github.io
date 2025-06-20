3e02dbc4f4605025ed2a3b2790ded711301fae5b
denoire <thomaszac19@gmail.com>
2025-06-19 17:56:41 -0700

    Add favicon and <title> tag

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+        <head>
+            <link rel="icon" href="/images/corridor-of-memories_icon.png"/>
+            <title>Thomas&apos; Personal Pages</title>
+        </head>
         <body>{children}</body>
         </html>
     );
